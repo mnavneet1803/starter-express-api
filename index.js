5c0b2c2ef204c1f13e04fabc32f032dec6815139
navneet <mnavneet315@gmail.com>
2023-09-03 11:29:04 +0530

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('you are at home page')
+})
+
+app.post("/signup",(req,res)=>{
+    console.log(req.body)
+    res.send({
+        status:true,
+        message:"signed up successfully",
+        data:req.body
+    })
 })
 app.listen(process.env.PORT || 3000)
