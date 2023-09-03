7ae3f775c2431d3611d6a68eb18082071ab816bf
navneet <mnavneet315@gmail.com>
2023-09-03 19:11:13 +0530

    mongodb setup

---
+++ config/db.config.js
@@ -0,0 +1,4 @@
+module.exports={
+    DB_NAME:"instagram",
+    DB_URL:"mongodb+srv://navneet:Navneet@cluster0.dky5xsn.mongodb.net/"
+}
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
-
+const mongoose = require("mongoose")
+const dbConfig = require("./config/db.config")
 app.use(express.json());
+  
+
+mongoose.connect(dbConfig.DB_URL)
+
+
+
+const db = mongoose.connection
+db.on("error", () => console.log("Can't connect to DB"))
+db.once("open", () => {
+    console.log("Connected to Mongo DB")
+    
+})
+
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
