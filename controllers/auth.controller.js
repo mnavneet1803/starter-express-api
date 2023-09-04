01f6aa816a3ed87570f8d953917451ca45ea33ef
Aman8996622 <amansh8996622@gmail.com>
2023-09-04 23:42:27 +0530

    sign work done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcryptjs")
 const User=require("../models/user.model")
+const jwt = require('jsonwebtoken')
+const config = require("../configs/auth.config")
 
 exports.signup = async (req,res)=>{
-    const user = {
+    const userObj = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         company_name: req.body.company_name,
@@ -12,7 +14,16 @@ exports.signup = async (req,res)=>{
         address: req.body.address
     }
     try {
-        const data = await User.create(user)
+        let userData = await User.find({ phone: userObj.phone })
+        if(userData.length){
+            res.send({
+                status:false,
+                message:"phone number already exist"
+            })
+        return
+        }
+        const data = await User.create(userObj)
+        console.log("user created successful")
         res.status(200).send({
             "message": "user created successful",
             data:data
@@ -24,4 +35,32 @@ exports.signup = async (req,res)=>{
         })
         
     }
+}
+
+exports.signin= async (req,res)=>{ 
+    const users = await User.find({ phone: req.body.phone })
+   if (users.length==0) {
+        res.status(400).send({
+            message: "Failed! Userid doesn't exist!"
+        })
+        return
+    }
+   if (!bcrypt.compareSync(req.body.password, users[0].password)) {
+        console.log("Condition met")
+        res.status(401).send({
+            message: "Invalid Password!"
+        })
+        return
+    }
+    let token = jwt.sign({phone: users[0].phone }, config.secret, {
+        expiresIn: 86400 // 24 hours
+    })
+    
+    res.status(200).send({
+        name:users[0].first_name,
+        phone:users[0].phone,
+        accessToken:token
+    })
+    
+    return
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,4 +2,5 @@
 const authController =require("../controllers/auth.controller")
 module.exports = function authRouter(app) {
     app.post("/auth/signup",authController.signup)
+    app.post("/auth/signin",authController.signin)
 }
